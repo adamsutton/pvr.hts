71ed4abc0dd7b62730aac43822a8886269906f5c
Sam Stenvall <sam.stenvall@nordsoftware.com>
2015-09-19 17:43:58 +0300

    reduce connection mutex lock scope in CHTSPVFS

--- src/Tvheadend.h
+++ src/Tvheadend.h
@@ -592,7 +592,6 @@ public:
   }
   inline void         VfsClose            ( void )
   {
-    PLATFORM::CLockObject lock(m_conn.Mutex());
     m_vfs.Close();
   }
   inline int          VfsRead             ( unsigned char *buf, unsigned int len )
